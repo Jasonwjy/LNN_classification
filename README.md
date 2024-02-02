d3d861f077fe760368bae5c92e9b84c988b21ce9
Jasonwjy <2362431944@qq.com>
2024-02-02 17:47:14 +0800

    commit

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
-暂时没东西写
+现在先xjb写一点，后面再重新整理
+代码都没写完呢
+
+# 基于液体神经网络的医学图像分类
+
+## 代码部分介绍
+
+
+### model.py
+
+------
+model.py包括一些模型，比如LTC1
+
+### 参考项目
++ LNN_cancer_classification
+项目地址：https://github.com/safipatel/LNN-cancer-classification
--- train.py
+++ train.py
@@ -24,6 +24,10 @@ transform = transforms.Compose([
 dataset = ImageFolder(root='train_data', transform=transform)
 data_loader = DataLoader(dataset, batch_size=16, shuffle=True)
 
+#加载验证集
+val_set = ImageFolder(root='val_data', transform=transform)
+val_loader = DataLoader(val_set, batch_size=16, shuffle=True)
+
 train_epoches = 10
 model = LTC1()
 model = model.to(device)
@@ -33,21 +37,52 @@ optimizer = torch.optim.Adam(model.parameters(), lr=0.001)
 loss_list = []
 
 for epoch in range(train_epoches):
+    train_loss, train_count = 0, 0
+    n_train_correct, n_train_samples = 0, 0
+    n_val_correct, n_val_samples = 0, 0
 
+    #训练
+    model.train()
     for x, label in tqdm(data_loader, unit='batch', desc='Running epoch {}'.format(epoch)):
         x = x.to(device)
         label = label.to(device)
 
         label = label.squeeze().long()
 
+        #前向传播，计算误差
         output = model(x)
         loss = criterion(output, label)
+
+        #记录当前批次的loss,acc
         loss_list.append(loss.item())
-        #predicted = torch.argmax(output.data, 1)
+        predicted = torch.argmax(output.data, 1)
+        n_train_samples += label.size(0)
+        n_train_correct += (predicted == label).sum().item()
 
         optimizer.zero_grad()
         loss.backward()
         optimizer.step()
+
+    #验证
+    model.eval()
+    for images, labels in val_loader:
+        images = images.to(device)
+        labels = labels.to(device)
+        labels = labels.squeeze().long()
+
+        outputs = model(images)
+        loss = criterion(outputs, labels)
+
+        predicted = torch.argmax(outputs.data, 1)
+
+        n_val_samples += labels.size(0)
+        n_val_correct += (predicted == labels).sum().item()
+
+    torch.save(model.state_dict(), 'save_model/model_after_epoch{}.model'.format(epoch))
+
+    print('train_acc:', n_train_correct/n_train_samples)
+    print('val_acc:', n_val_correct/n_val_samples)
+
 
 
 
